74fbc002bfe55b7f091cd769e8fb98f551e4e08a
Kato Hidetoshi <maildhl@digital-hub-app.work>
2020-12-31 08:32:13 +0900

    Initial Commit

---
+++ LearningRecord/LearningRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningRecordApp.swift
+//  LearningRecord
+//
+//  Created by Digital Hub Life on 2020/12/31.
+//
+
+import SwiftUI
+
+@main
+struct LearningRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
